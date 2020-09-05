f8a4f2d3cbf4754243e8a1fdb33f81055e00af5a
kuwabara soma <kuwabara.soma@run-edge.com>
2020-09-06 02:55:51 +0900

    porpsのテストコードを追加

---
+++ src/components/PropsTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Propsの値は {{ props.text.val }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+type Props = Readonly<{
+  text?: {
+    val?: string
+  }
+}>
+
+export default defineComponent({
+  name: 'PropsTest',
+  props: {
+    text: Object,
+  },
+  setup(props: Props) {
+    return {
+      props,
+    }
+  },
+})
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/page/TestPage2.vue
+++ src/views/page/TestPage2.vue
@@ -1,15 +1,18 @@
 <template>
   <display-text />
+  <props-test :text="{ val: 'Propsの入力テストだよ' }" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import DisplayText from '@/components/DisplayText.vue'
+import PropsTest from '@/components/PropsTest.vue'
 
 export default defineComponent({
   name: 'TestPage2',
   components: {
     DisplayText,
+    PropsTest,
   },
 })
 </script>
